9f036afb508a6e9f38eeac80f45db51edf620473
Konab <konab-job@yandex.ru>
2019-04-20 12:09:25 +0300

    Big update

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,8 +1,13 @@
-from flask import render_template, flash
+import app
 from app import db
 from app.main import bp
-# from app.main.forms import PostForm
-from app.models import Schools
+from app.models import Schools, GeoData
+from flask import render_template, flash, jsonify, request
+from sqlalchemy import func
+
+
+
+MAPBOX_KEY = app.Config.MAPBOX_KEY
 
 
 @bp.route('/', methods=['GET', 'POST'])
@@ -16,5 +21,24 @@ def index():
 	# 	db.session.close()
 	# 	flash("It's work!")
 	name = 'МИР'
-	name = Schools.query.filter_by(id=1).first().chiefname
+	# name = Schools.query.filter_by(id=1).first().chiefname
+	# name = Super.query.filter_by(id=7).first().geom
+	pt = GeoData.query.get(1)
+	name = db.session.scalar(func.ST_AsGeoJSON(pt.geom)) # Получаем координаты в формате JSON
 	return render_template('index.html', name=name, title='Moscow')
+
+@bp.route('/')
+@bp.route('/hello')
+def hello():
+	return render_template('hello.html')
+
+
+@bp.route('/map')
+def mapping():
+	# return jsonify({'test': 1})
+	return render_template('map.html', ACCESS_KEY=MAPBOX_KEY)
+
+
+@bp.route('/get_gis', methods=['POST'])
+def get_gis():
+	return jsonify({'text': 'hello world'})
